6cb8faefcacc6215852cdf7d55eeaf00f329719f
Flyon321 <76116045+Flyon321@users.noreply.github.com>
2022-04-07 12:31:32 +0200

    Create Microsoft_Rewards.py

---
+++ Microsoft_Rewards.py
@@ -0,0 +1,73 @@
+import pyautogui as pg
+import time
+import subprocess as sb
+
+def close_tab():
+    with pg.hold('ctrl'):
+        pg.press('PgUp')
+    time.sleep(0.5)
+    with pg.hold('ctrl'):
+        pg.press('w')
+
+def write():
+    time.sleep(2)
+    pg.typewrite(phrase, 0.03)
+    time.sleep(0.5)
+    pg.press('enter')
+    time.sleep(3)
+
+def change_device():
+    with pg.hold('ctrl'):
+        with pg.hold('shift'):
+            pg.press('m')
+    time.sleep(2)
+
+
+count_x = 0
+count_y = 0
+x = int(input("Enter the number of searches to do on Windows: "))
+y = int(input("Enter the number of searches to do on Mobile: "))
+
+#ENTER YOUR FIREFOX PATH WITH BING SEARCH ENGINE
+sb.Popen("PATH")
+
+file_windows = open("site.txt")
+phrase = file_windows.readline
+
+time.sleep(6)
+for phrase in file_windows:
+    if count_x < x:
+        write()
+
+        with pg.hold('ctrl'):
+            pg.press('t')
+
+        time.sleep(0.5)
+        close_tab()
+
+        count_x = count_x + 1
+    else:
+        break
+
+file_windows.close
+
+file_mobile = open("site.txt")
+phrase = file_mobile.readline
+
+for phrase in file_mobile:
+    if count_y < y:
+        change_device()
+
+        write()
+
+        with pg.hold('ctrl'):
+            pg.press('t')
+
+        time.sleep(0.5)
+        close_tab()
+
+        count_y = count_y + 1
+    else:
+        break
+
+file_mobile.close
